a1fb034032cb5fddc734ab7fa1ff009674fc35fa
MusaTamzid05 <tamzid101@gmail.com>
2025-02-08 21:11:33 +0600

    Create an util pack func

--- lib/utils.go
+++ lib/utils.go
@@ -2,6 +2,9 @@ package lib
 
 import (
     "os"
+    "bytes"
+    "encoding/binary"
+    "fmt"
 )
 
 func Exists(path string) bool {
@@ -12,4 +15,38 @@ func Exists(path string) bool {
     }
 
     return true
+}
+
+func pack(magicStr string, number uint16, entries uint32) (string, error){
+    // create buffer for the magic string, add padding if 
+    // magic string is less than 4 
+    // save the number as big endian in buffer
+    // save the entries as big endian in buffer
+    // return buffer as string
+
+
+    buffer := new(bytes.Buffer)
+    magicBytes := []byte(magicStr)
+
+    for i := len(magicBytes); i < 4; i += 1 {
+        magicBytes = append(magicBytes, 0)
+    }
+
+
+    err := binary.Write(buffer, binary.BigEndian, number)
+
+    if err != nil {
+        return  "", fmt.Errorf("Error packing number as BigEndian ", err)
+    }
+
+    err = binary.Write(buffer, binary.BigEndian, entries)
+
+    if err != nil {
+        return  "", fmt.Errorf("Error packing entries as BigEndian ", err)
+    }
+
+    bytesData := buffer.Bytes()
+
+    return string(bytesData), nil
+
 }
